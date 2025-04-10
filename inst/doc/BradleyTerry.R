a6ce24eeadb1eafe36c336c27567a47e9d8e2e7a
Heather Turner <ht@heatherturner.net>
2025-04-10 19:20:44 +0000

    version 1.1.3

--- inst/doc/BradleyTerry.R
+++ inst/doc/BradleyTerry.R
@@ -1,116 +1,73 @@
-### R code from vignette source 'BradleyTerry.Rnw'
-### Encoding: UTF-8
+## ----include=FALSE------------------------------------------------------------
+library <- function(...) suppressPackageStartupMessages(base::library(...))
+library(knitr)
+opts_chunk$set(
+tidy=FALSE
+)
 
-###################################################
-### code chunk number 1: set_options
-###################################################
+## ----set_options, echo = FALSE--------------------------------------
 options(prompt = "R> ", continue = "+  ", width = 70,
         useFancyQuotes = FALSE, digits = 7)
 
-
-###################################################
-### code chunk number 2: LoadBradleyTerry2
-###################################################
+## ----LoadBradleyTerry2----------------------------------------------
 library("BradleyTerry2")
 
-
-###################################################
-### code chunk number 3: CitationData
-###################################################
+## ----CitationData---------------------------------------------------
 data("citations", package = "BradleyTerry2")
 
-
-###################################################
-### code chunk number 4: CitationData2
-###################################################
+## ----CitationData2--------------------------------------------------
 citations
 
-
-###################################################
-### code chunk number 5: countsToBinomial
-###################################################
+## ----countsToBinomial-----------------------------------------------
 citations.sf <- countsToBinomial(citations)
 names(citations.sf)[1:2] <- c("journal1", "journal2")
 citations.sf
 
-
-###################################################
-### code chunk number 6: citeModel
-###################################################
+## ----citeModel------------------------------------------------------
 citeModel <- BTm(cbind(win1, win2), journal1, journal2, ~ journal,
     id = "journal", data = citations.sf)
 citeModel
 
-
-###################################################
-### code chunk number 7: citeModelupdate
-###################################################
+## ----citeModelupdate------------------------------------------------
 update(citeModel, refcat = "JASA")
 
-
-###################################################
-### code chunk number 8: citeModelupdate2
-###################################################
+## ----citeModelupdate2-----------------------------------------------
 update(citeModel, br = TRUE)
 
-
-###################################################
-### code chunk number 9: lizModel
-###################################################
+## ----lizModel-------------------------------------------------------
 options(show.signif.stars = FALSE)
 data("flatlizards", package = "BradleyTerry2")
 lizModel <- BTm(1, winner, loser, ~ SVL[..] + (1|..),
                 data = flatlizards)
 
-
-###################################################
-### code chunk number 10: summarize_lizModel
-###################################################
+## ----summarize_lizModel---------------------------------------------
 summary(lizModel)
 
-
-###################################################
-### code chunk number 11: lizModel2
-###################################################
+## ----lizModel2------------------------------------------------------
 lizModel2 <- BTm(1, winner, loser,
             ~ throat.PC1[..] + throat.PC3[..] +
             head.length[..] + SVL[..] + (1|..),
             data = flatlizards)
 summary(lizModel2)
 
-
-###################################################
-### code chunk number 12: baseball
-###################################################
+## ----baseball-------------------------------------------------------
 data("baseball", package = "BradleyTerry2")
 head(baseball)
 
-
-###################################################
-### code chunk number 13: baseballModel
-###################################################
+## ----baseballModel--------------------------------------------------
 baseballModel1 <- BTm(cbind(home.wins, away.wins), home.team, away.team,
                       data = baseball, id = "team")
 summary(baseballModel1)
 
-
-###################################################
-### code chunk number 14: baseballDataUpdate
-###################################################
+## ----baseballDataUpdate---------------------------------------------
 baseball$home.team <- data.frame(team = baseball$home.team, at.home = 1)
 baseball$away.team <- data.frame(team = baseball$away.team, at.home = 0)
 
-
-###################################################
-### code chunk number 15: baseballModelupdate
-###################################################
+## ----baseballModelupdate--------------------------------------------
 baseballModel2 <- update(baseballModel1, formula = ~ team + at.home)
 summary(baseballModel2)
 
-
-###################################################
-### code chunk number 16: CEMSmodel
-###################################################
+## ----CEMSmodel------------------------------------------------------
 data("CEMS", package = "BradleyTerry2")
 table8.model <-  BTm(outcome = cbind(win1.adj, win2.adj),
     player1 = school1, player2 = school2, formula = ~ .. +
@@ -122,77 +79,50 @@ table8.model <-  BTm(outcome = cbind(win1.adj, win2.adj),
     SEX[student] * Milano[..],
     refcat = "Stockholm", data = CEMS)
 
-
-###################################################
-### code chunk number 17: BTabilities
-###################################################
+## ----BTabilities----------------------------------------------------
 BTabilities(baseballModel2)
 
-
-###################################################
-### code chunk number 18: BTabilities2
-###################################################
+## ----BTabilities2---------------------------------------------------
 head(BTabilities(lizModel2), 4)
 
+## ----figqvplot, echo=FALSE , fig.cap="Estimated relative abilities of baseball teams.", fig.alt="The ability for Baltimore is fixed at zero, with an interval ranging from -0.5 to 0.5. Boston has a relative ability near 1.2; Cleveland around 0.7. The remaining teams have relative abilities around 1.3 to 1.6. The intervals are based on quasi standard errors and all have length of approximately 1. Therefore, aside from Cleveland, all teams are clearly significantly stronger than Baltimore as the intervals do not overlap.", fig.show='hold', fig.align="center", out.width="67.0%"----
+knitr::include_graphics(c("baseball-qvplot.png"))
 
-###################################################
-### code chunk number 19: residuals
-###################################################
+## ----residuals------------------------------------------------------
 res.pearson <- round(residuals(lizModel2), 3)
 head(cbind(flatlizards$contests, res.pearson), 4)
 
-
-###################################################
-### code chunk number 20: BTresiduals
-###################################################
+## ----BTresiduals----------------------------------------------------
 res <- residuals(lizModel2, type = "grouped")
 #  with(flatlizards$predictors, plot(throat.PC2, res))
 #  with(flatlizards$predictors, plot(head.width, res))
 
-
-###################################################
-### code chunk number 21: residualWLS
-###################################################
+## ----residualWLS----------------------------------------------------
 lm(res ~ throat.PC1, weights = attr(res, "weights"),
    data = flatlizards$predictors)
 lm(res ~ head.length, weights = attr(res, "weights"),
    data = flatlizards$predictors)
 
+## ----figresiduals, echo=FALSE , fig.cap="Lizard residuals for the simple model lizModel, plotted against throat.PC3.", fig.alt="The residuals are quite spread out over the range -2 to 2, but the distribution is clearly not uniform over the range of the predictor variable, throat.PC3. Residuals between -2 and -1 range correspond to values throat.PC3 between -6 and 4; residuals between -1 and 1 correspond to throat.PC3 values of -4 to 4, and residuals from 1 to 2 correspond to throat.PC3 values between -3 and 6. Thus there is an overall positive correlation bewteen the residuals and throat.PC3.", fig.show='hold', fig.align="center", out.width="69.0%"----
+knitr::include_graphics(c("residuals.png"))
 
-###################################################
-### code chunk number 22: baseballModel2_call
-###################################################
+## ----baseballModel2_call--------------------------------------------
  baseballModel2$call
 
-
-###################################################
-### code chunk number 23: str_baseball
-###################################################
+## ----str_baseball---------------------------------------------------
 str(baseball, vec.len = 2)
 
-
-###################################################
-### code chunk number 24: first_comparison
-###################################################
+## ----first_comparison-----------------------------------------------
 baseball$home.team[1,]
 baseball$away.team[1,]
 
-
-###################################################
-### code chunk number 25: first_outcome
-###################################################
+## ----first_outcome--------------------------------------------------
  baseball[1, c("home.wins", "away.wins")]
 
-
-###################################################
-### code chunk number 26: str_CEMS
-###################################################
+## ----str_CEMS-------------------------------------------------------
 str(CEMS, vec.len = 2)
 
-
-###################################################
-### code chunk number 27: student-specific_data
-###################################################
+## ----student-specific_data------------------------------------------
 library("prefmod")
 student <- cemspc[c("ENG", "SEX")]
 student$ENG <- factor(student$ENG, levels = 1:2,
@@ -200,43 +130,28 @@ student$ENG <- factor(student$ENG, levels = 1:2,
 student$SEX <- factor(student$SEX, levels = 1:2,
                       labels = c("female", "male"))
 
-
-###################################################
-### code chunk number 28: student_factor
-###################################################
+## ----student_factor-------------------------------------------------
 cems <- list(student = student)
 student <- gl(303, 1, 303 * 15) #303 students, 15 comparisons
 contest <- data.frame(student = student)
 
-
-###################################################
-### code chunk number 29: binomial_response
-###################################################
+## ----binomial_response----------------------------------------------
 win <- cemspc[, 1:15] == 0
 lose <- cemspc[, 1:15] == 2
 draw <- cemspc[, 1:15] == 1
 contest$win.adj <- c(win + draw/2)
 contest$lose.adj <- c(lose + draw/2)
 
-
-###################################################
-### code chunk number 30: school_factors
-###################################################
+## ----school_factors-------------------------------------------------
 lab <- c("London", "Paris", "Milano", "St. Gallen", "Barcelona",
          "Stockholm")
 contest$school1 <- factor(sequence(1:5), levels = 1:6, labels = lab)
 contest$school2 <- factor(rep(2:6, 1:5), levels = 1:6, labels = lab)
 
-
-###################################################
-### code chunk number 31: cems_data
-###################################################
+## ----cems_data------------------------------------------------------
 cems$contest <- contest
 
-
-###################################################
-### code chunk number 32: functions
-###################################################
+## ----functions, echo = FALSE------------------------------
 ## cf. prompt
 options(width = 55)
 for (fn in getNamespaceExports("BradleyTerry2")) {
@@ -259,5 +174,4 @@ for (fn in getNamespaceExports("BradleyTerry2")) {
     cat(deparse(parse(text = Call)[[1]], width.cutoff = 50), fill = TRUE)
 }
 options(width = 60)
-
 
